a3cf02db80bb3660decbacf9eefd4b3078bfff6d
Juani Reyes <juanisoul@gmail.com>
2021-08-16 22:57:29 -0400

    Add textarea and button templates

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.scss'
+import Head from "next/head";
+import styles from "../styles/Home.module.scss";
 
 export default function Home() {
   return (
@@ -11,31 +11,37 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          My Posts
-        </h1>
+        <h1 className={styles.title}>My Posts</h1>
 
         <ul className={styles.posts}>
           <li>
             <p className={styles.postsContent}>
-              I’m working in Figma trying to design a new website that shows all of my tweets!
+              I’m working in Figma trying to design a new website that shows all
+              of my tweets!
             </p>
             <p className={styles.postsDate}>02/26/2021</p>
           </li>
           <li>
             <p className={styles.postsContent}>
-              I’m working in Figma trying to design a new website that shows all of my tweets!
+              I’m working in Figma trying to design a new website that shows all
+              of my tweets!
             </p>
             <p className={styles.postsDate}>02/26/2021</p>
           </li>
           <li>
             <p className={styles.postsContent}>
-              I’m working in Figma trying to design a new website that shows all of my tweets!
+              I’m working in Figma trying to design a new website that shows all
+              of my tweets!
             </p>
             <p className={styles.postsDate}>02/26/2021</p>
           </li>
         </ul>
+
+        <form>
+          <textarea className={styles.formContent}></textarea>
+          <button className={styles.formButton}>Add New Post</button>
+        </form>
       </main>
     </div>
-  )
+  );
 }
