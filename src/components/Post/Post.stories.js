cf549f1628dd11fcb0106c5d159635e61492e6c6
Juani Reyes <juanisoul@gmail.com>
2021-08-19 23:35:51 -0400

    Add global styles to storybook and props to Post.stories

--- src/components/Post/Post.stories.js
+++ src/components/Post/Post.stories.js
@@ -2,6 +2,12 @@ import React from "react";
 
 import Post from "./Post";
 
+const DUMMY_POST = {
+  content:
+    "I’m working in Figma trying to design a new website that shows allof my tweets!",
+  date: "02/26/2021",
+};
+
 export default {
   title: "Example/Post",
   component: Post,
@@ -10,3 +16,6 @@ export default {
 const Template = (args) => <Post {...args} />;
 
 export const Primary = Template.bind({});
+Primary.args = {
+  ...DUMMY_POST,
+};
