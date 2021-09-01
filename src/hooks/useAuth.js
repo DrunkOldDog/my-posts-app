28936980ec6ae542ca26a5ccc3034c99d5d6796c
Juani Reyes <juanisoul@gmail.com>
2021-08-31 22:21:05 -0400

    Add user, logIn and logOut methods to AuthProvider

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -1,9 +1,43 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
+
+import {
+  auth,
+  init,
+  logIn as authLogIn,
+  logOut as authLogOut,
+} from "../lib/auth";
 
 export const AuthContext = createContext();
 
 export default function AuthProvider({ children }) {
-  return <AuthContext.Provider value={{}}>{children}</AuthContext.Provider>;
+  const [user, setUser] = useState();
+
+  useEffect(() => {
+    init(setUser);
+    auth.on("login", setUser);
+
+    return () => {
+      auth.off("login", setUser);
+    };
+  }, []);
+
+  function logIn() {
+    authLogIn(setUser);
+  }
+
+  function logOut() {
+    authLogOut(setUser);
+  }
+
+  const contextValue = {
+    user,
+    logIn,
+    logOut,
+  };
+
+  return (
+    <AuthContext.Provider value={contextValue}>{children}</AuthContext.Provider>
+  );
 }
 
 export const useAuth = () => {
